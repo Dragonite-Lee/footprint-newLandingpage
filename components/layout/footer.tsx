d4c05fa102bb10446d874ba54763ddcd990133dd
Dragonite-Lee <ross991222@gmail.com>
2023-06-23 15:25:47 +0900

    footer 1차 초안 반응형과 링크연결 필요

---
+++ components/layout/footer.tsx
@@ -0,0 +1,32 @@
+import styles from './layout.module.css'
+import facebook from '../../public/layout/facebook.png'
+import instagram from '../../public/layout/instagram.png'
+import footer_left from '../../public/layout/footer_left.png'
+import footer_right_bussiness from '../../public/layout/footer_right_bussiness.png'
+import footer_right_contact from '../../public/layout/footer_right_contact.png'
+
+import Link from 'next/link';
+import Image from 'next/image';
+ 
+function Footer() {
+
+  return (
+      <div className={styles.footer}>
+        <div>
+            <Image src={footer_left} className={styles.footer_left_img} alt='footer_left이미지' />
+        </div>
+        <div className={styles.footer_right}>
+            <div className={styles.footer_right_contact}>
+            <Image src={footer_right_contact} className={styles.footer_right_contact_img} alt='footer_right_contact이미지' />
+            <Image src={instagram} className={styles.instagram} alt='instagram이미지' />
+            <Image src={facebook} className={styles.facebook} alt='facebook이미지' />
+            </div>
+            <div className={styles.footer_right_bussiness}>
+            <Image src={footer_right_bussiness} className={styles.footer_right_bussiness_img} alt='footer_right_bussiness이미지' />
+            </div>
+        </div>
+      </div>
+  )
+}
+
+export default Footer;
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,7 @@
 import styles from './layout.module.css'
 import navbarLogo from '../../public/layout/navbar_logo.png'
+import Header from './header';
+import Footer from './footer';
 
 import Link from 'next/link';
 import Image from 'next/image';
@@ -12,24 +14,9 @@ function Layout({ children }: AppLayoutProps) {
 
   return (
     <div>
-      <div className={styles.navbar}>
-        <Link href="/" className={styles.navbar_left}>
-          <Image src={navbarLogo} className={styles.navbar_logo} alt='navbar_logo이미지' />
-        </Link>
-        <div className={styles.navbar_right}>
-          <Link href="/Intro">
-            <div className={styles.navbar_intro}>
-              <span className='font_bold'>Footprint</span> 소개
-            </div>
-          </Link>
-          <Link href="/Ongoing">
-            <div className={styles.navbar_ongoing}>
-              연재 중인 스토리
-            </div>
-          </Link>
-        </div>
-      </div>
+      <Header />
       <main>{children}</main>
+      <Footer />
     </div>
   )
 }
