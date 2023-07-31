e5614f15646110f47aae42057a181ee69334f7b2
dragonite-lee <ross991222@gmail.com>
2023-07-31 21:46:14 +0900

    PC뷰부터 수정하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image';
 
 import styles from '../styles/Home.module.css'
-import Introduce from '../components/main/introduce';
-import Yellow from '../components/main/yellow';
-import But from '../components/main/but';
-import Gray from '../components/main/gray';
-import Curious from '../components/main/curious';
-import Together from '../components/main/together';
-import Qna from '../components/main/qna';
+import Introduce from '../components/main/introduce'
+import Yellow from '../components/main/yellow'
+import But from '../components/main/but'
+import Gray from '../components/main/gray'
+import Curious from '../components/main/curious'
+import Together from '../components/main/together'
+import Qna from '../components/main/qna'
+import bg_main_pc from "../public/bg/bg_main_pc.png"
 
 const Home: NextPage = () => {
   
-
   return (
     <div>
       <Head>
@@ -41,6 +42,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div className={styles.container}>
+          <Image src={bg_main_pc} alt='bg_main_pc' className={styles.bg_main_pc} />
           <Introduce top_text='하늬님의 스토리 보러가기' bot_text={"국내 패션 커머스 기업에서 서버 개발자의 삶을 살아가고 있는 주인공 '하늬'는 딱히 좋아하고 \n잘하는 것도 없었어요. 기대감에 젖어 진학한 컴공과마저 적성에 맞지 않아 포기하려 하기도 했죠. \n그런 그녀가 서버 개발자의 삶을 선택하고 달성하기까지의 치열한 여정을 다루고 있습니다."} />
           <Yellow />
           <But />
